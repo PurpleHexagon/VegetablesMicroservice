1365bca66c762ae9f9cb86464493e57f95c88d1f
Jon Day <jon@plantigrate.com>
2020-07-21 11:14:47 +0100

    Alter vegetables route

--- src/Actions/GetVegetablesAction.php
+++ src/Actions/GetVegetablesAction.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-namespace PurpleHexagon\Http;
+namespace PurpleHexagon\Actions;
 
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
@@ -30,7 +30,7 @@ class GetVegetablesAction
     }
 
     /**
-     * GET /
+     * GET /vegetables
      *
      * @return ResponseInterface
      */
